77171051ee67dde20cbee395cfd10064fc34a333
st3phy831 <st3phy831@gmail.com>
2020-10-09 13:57:38 -0700

    Added character count for Tweet

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -3,15 +3,19 @@ package com.codepath.apps.restclienttemplate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ComposeActivity extends AppCompatActivity {
-    public static final int MAX_TWEET_LENGTH = 140;
+    public static final int MAX_TWEET_LENGTH = 280;
     EditText etCompose;
     Button btnTweet;
+    TextView tvCharCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +24,31 @@ public class ComposeActivity extends AppCompatActivity {
 
         etCompose = findViewById(R.id.etCompose);
         btnTweet = findViewById(R.id.btnTweet);
+        tvCharCount = findViewById(R.id.tvCharCount);
 
+        etCompose.addTextChangedListener(new TextWatcher() {
+	        @Override
+	        public void onTextChanged(CharSequence s, int start, int before, int count) {
+		        // Fires right as the text is being changed (even supplies the range of text)
+	        }
+	        @Override
+	        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+		        // Fires right before text is changing
+	        }
+	        @Override
+	        public void afterTextChanged(Editable s) {
+		        // Fires right after the text has changed
+                int length = MAX_TWEET_LENGTH - s.length();
+                if (length >= 0){
+                    String stringLength = (String.valueOf(length));
+                    tvCharCount.setText(stringLength);
+                    btnTweet.setEnabled(true);
+                }else{
+                    btnTweet.setEnabled(false);
+                }
+
+	        }
+        });
         //Set click listener on button
         btnTweet.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,10 +56,6 @@ public class ComposeActivity extends AppCompatActivity {
                 String tweetContent = etCompose.getText().toString();
                 if (tweetContent.isEmpty()){
                     Toast.makeText(ComposeActivity.this, "Sorry, Tweet cannot be empty", Toast.LENGTH_LONG).show();
-                    return;
-                }
-                if(tweetContent.length() > MAX_TWEET_LENGTH){
-                    Toast.makeText(ComposeActivity.this, "Sorry, Tweet id too long", Toast.LENGTH_LONG).show();
                     return;
                 }
                 Toast.makeText(ComposeActivity.this, tweetContent, Toast.LENGTH_LONG).show();
